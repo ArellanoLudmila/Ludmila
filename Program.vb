7e16825fbd42b3cecab882a6747f8d703a5182da
ArellanoLudmila <140514531+ArellanoLudmila@users.noreply.github.com>
2023-07-27 08:36:31 -0300

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,36 @@
+Imports System
+Module Program
+    Sub Main(args As String())
+        Dim Variable As 
+        Console.WriteLine("este es un texto a string DEL MENU")
+        Do Until Variable = 4
+            Console.WriteLine("Esta es la opcion 1")
+            Console.WriteLine("Esta es la opcion 2")
+            Console.WriteLine("Opcion 3")
+            Console.WriteLine(salir)
+            Variable = Console.ReadLine
+            If Variable = 1 Then
+                Console.WriteLine("Esto es opcion 1")
+            ElseIf Variable = 2 Then
+                Console.WriteLine("Esto es opcion2")
+            ElseIf Variable = 4 Then
+                Console.WriteLine("Esto es opcion3")
+            End If
+            Console.WriteLine(Estoes opcion4")
+
+
+
+
+
+            End If
+
+
+
+
+
+
+
+
+        Loop
+    End Sub
+End Module
